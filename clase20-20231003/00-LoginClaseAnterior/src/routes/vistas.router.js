8366352d01689ea5643377d89699946a9859e41e
eruedasanchez <ezequiel.ruedasanchez@gmail.com>
2023-10-10 14:47:35 -0300

    [fix]: Se eliminan variables parala visualizacion de alertas en las vista de registro y login

--- clase20-20231003/00-LoginClaseAnterior/src/routes/vistas.router.js
+++ clase20-20231003/00-LoginClaseAnterior/src/routes/vistas.router.js
@@ -34,41 +34,72 @@ router.get('/',(req,res)=>{
 
 router.get('/registro',auth2,(req,res)=>{
 
-    let error=false
-    let errorDetalle=''
-    if(req.query.error){
-        error=true
-        errorDetalle=req.query.error
-    }
+    // let error=false
+    // let errorDetalle='';
 
+    // if(req.query.error){
+    //     // error=true
+    //     errorDetalle=req.query.error;
+    // }
+    
+    // res.status(200).render('registro',{
+    //     verLogin:true,
+    //     errorDetalle
+    //     // error,
+    // })
+    
+    
+    // [fix]: Podemos notar que en JS un string cualquiera no vacio es considerado true
+    // Esto nos permite declarar la variable error
+    let errorDetalle='';
+    
+    if(req.query.error) errorDetalle=req.query.error;
+    
     res.status(200).render('registro',{
         verLogin:true,
-        error, errorDetalle
+        errorDetalle
     })
 })
 
 router.get('/login',auth2,(req,res)=>{
 
-    let error=false
-    let errorDetalle=''
-    if(req.query.error){
-        error=true
-        errorDetalle=req.query.error
-    }
+    // // let error=false
+    // let errorDetalle=''
+    // if(req.query.error){
+    //     // error=true
+    //     errorDetalle=req.query.error
+    // }
 
-    let usuarioCreado=false
-    let usuarioCreadoDetalle=''
-    if(req.query.usuarioCreado){
-        usuarioCreado=true
-        usuarioCreadoDetalle=req.query.usuarioCreado
-    }
+    // // let usuarioCreado=false
+    // let usuarioCreadoDetalle=''
+    // if(req.query.usuarioCreado){
+    //     // usuarioCreado=true
+    //     usuarioCreadoDetalle=req.query.usuarioCreado
+    // }
 
 
 
+    // res.status(200).render('login',{
+    //     verLogin:true,
+    //     // error, usuarioCreado, 
+    //     usuarioCreadoDetalle,
+    //     errorDetalle
+    // })
+
+    // [fix]: Mismo que en el caso del registro de usuarios
+
+    let errorDetalle='', usuarioCreadoDetalle='';
+
+    let {error, usuarioCreado} = req.query;
+    
+    if(error) errorDetalle = error;
+    
+    if(usuarioCreado) usuarioCreadoDetalle = usuarioCreado;
+    
     res.status(200).render('login',{
         verLogin:true,
-        usuarioCreado, usuarioCreadoDetalle,
-        error, errorDetalle
+        errorDetalle, 
+        usuarioCreadoDetalle
     })
 })
 
